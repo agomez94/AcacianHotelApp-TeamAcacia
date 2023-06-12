3cdded87797e47637af71e389d6cb5fc8d0db709
GabeD23 <130799642+GabeD23@users.noreply.github.com>
2023-06-12 13:26:36 -0500

    Update lambda-save.py
    adding lambda function file to src folder

--- ServerlessRealEstateApp-TeamAcacia/src/python-function/lambda-save.py
+++ ServerlessRealEstateApp-TeamAcacia/src/python-function/lambda-save.py
@@ -1,1 +1,43 @@
 #can be used for the lambda function code to save user hotel selections
+import json
+import boto3
+import mysql.connector
+
+def lambda_handler(event, context): 
+    # Extract username and password from the API request body
+    request_body = json.loads(event['body'])
+    username = request_body['username']
+    password = request_body['password']
+
+    # Create a connection to the RDS MySQL database
+    db_instance_identifier = 'your-db-instance-identifier'  # Replace with your RDS instance identifier
+    db_endpoint = boto3.client('rds').describe_db_instances(DBInstanceIdentifier=db_instance_identifier)['DBInstances'][0]['Endpoint']['Address']
+
+    # Connect to the database and insert the username and password
+    
+    try:
+        conn = mysql.connector.connect(host=db_endpoint, user='your-db-username', password='your-db-password', database='your-db-name')
+        cursor = conn.cursor()
+
+        # Execute the INSERT query
+        insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        cursor.execute(insert_query, (username, password))
+        conn.commit()
+
+        # Close the database connection
+        cursor.close()
+        conn.close()
+
+        # Return a success response
+        response = {
+            'statusCode': 200,
+            'body': json.dumps('Username and password saved successfully')
+        }
+    except mysql.connector.Error as error:
+        # Return an error response
+        response = {
+            'statusCode': 500,
+            'body': json.dumps(f"Error saving username and password: {str(error)}")
+        }
+
+    return response
