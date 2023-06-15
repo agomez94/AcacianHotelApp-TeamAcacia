ad957c1b7b305f2316f0710e9b67e210d80e1a69
[Kell-s97] <[gordonkelley97@gmail.com]>
2023-06-15 13:58:24 -0500

    login works

--- ServerlessRealEstateApp-TeamAcacia/src/python-function/lambda-save.py
+++ ServerlessRealEstateApp-TeamAcacia/src/python-function/lambda-save.py
@@ -3,44 +3,44 @@ import boto3
 import mysql.connector
 
 def lambda_handler(event, context):
-    # Extract username and password from the API request body
-    username = event['username']
-    password = event['password']
+    # Extract UserID and Password from the API request body
+    UserID = event['UserID']
+    Password = event['Password']
 
     # Create a connection to the RDS MySQL database
-    db_instance_identifier = 'tf-20230610192157489100000003'  # Replace with your RDS instance identifier
+    db_instance_identifier = 'tf-20230614154028687200000003'  # Replace with your RDS instance identifier
     db_endpoint = boto3.client('rds').describe_db_instances(DBInstanceIdentifier=db_instance_identifier)['DBInstances'][0]['Endpoint']['Address']
 
-    # Connect to the database and check if the username already exists
-    conn = mysql.connector.connect(host=db_endpoint, user='taadmin1', password='12qwaszx#$ERDFCV', database='team-acacia-test-db-cluster')
+    # Connect to the database and check if the UserID already exists
+    conn = mysql.connector.connect(host=db_endpoint, user='taadmin1', password='12qwaszx#$ERDFCV', database='Team-Acacia-DB')
     cursor = conn.cursor()
-    select_query = "SELECT username FROM users WHERE username = %s"
-    cursor.execute(select_query, (username,))
-    existing_username = cursor.fetchone()
+    select_query = "SELECT UserID FROM UserInfo WHERE UserID = %s"
+    cursor.execute(select_query, (UserID,))
+    existing_UserID = cursor.fetchone()
 
-    if existing_username:
-        # Username already exists, return an error response
+    if existing_UserID:
+        # UserID already exists, return an error response
         response = {
             'statusCode': 400,
-            'body': json.dumps('Username already exists')
+            'body': json.dumps('UserID already exists')
         }
     else:
         try:
-            # Execute the INSERT query to save the username and password
-            insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
-            cursor.execute(insert_query, (username, password))
+            # Execute the INSERT query to save the UserID and Password
+            insert_query = "INSERT INTO UserInfo (UserID, Password) VALUES (%s, %s)"
+            cursor.execute(insert_query, (UserID, Password))
             conn.commit()
 
             # Return a success response
             response = {
                 'statusCode': 200,
-                'body': json.dumps('Username and password saved successfully')
+                'body': json.dumps('UserID and Password saved successfully')
             }
         except mysql.connector.Error as error:
             # Return an error response
             response = {
                 'statusCode': 500,
-                'body': json.dumps(f"Error saving username and password: {str(error)}")
+                'body': json.dumps(f"Error saving UserID and Password: {str(error)}")
             }
 
     # Close the database connection
@@ -48,3 +48,13 @@ def lambda_handler(event, context):
     conn.close()
 
     return response
+
+json_data = {}
+json_data["UserID"] = "test_username"
+json_data["Password"] = "password"
+data = json_data
+
+
+
+
+lambda_handler(data, None)
